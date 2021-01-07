d2a650e6bc1660ea1d6e0584f0b44cf95e787d4b
1261954 <hengxie.che@oborcloud.com>
2021-01-07 23:41:15 +0800

    1

--- src/main/java/com/chea/cinema/controller/ContentController.java
+++ src/main/java/com/chea/cinema/controller/ContentController.java
@@ -1,64 +1,51 @@
 package com.chea.cinema.controller;
+ 
 
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class ContentController {
 
-	@Autowired
-	private NonStaticResourceHttpRequestHandler nonStaticResourceHttpRequestHandler;
-
-	@GetMapping("/mp4")
-	public String mp4(@RequestParam String id, HttpServletResponse response) {
-
-		System.out.println("Hello");
-
-		return "Hello" + id;
-
-	}
 
 	@RequestMapping("/hello")
 	public String index() {
 		return "hello";
 	}
 
-//	  @RequestMapping("/mp4/{name}")
-//	    public String mp4(@PathVariable("name") String name,@RequestParam String sign,HttpServletResponse response) {
-//		  
-//		  
-//		  
-//		  return "Hello"+name+"-sign"+ sign; 
-//	  }
-	@RequestMapping("/mp4/{name}.mp4")
-	public void hello(@PathVariable("name") String name, @RequestParam String sign, HttpServletRequest request,
+
+ 
+ 
+	@RequestMapping("/movie/{name}.m3u8")
+	public void  m(@PathVariable("name") String name, @RequestParam String sign,HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
-		MinioClient minioClient = new MinioClient("http://39.152.129.170:9000", "admin", "j5811227");
+	    URL url = null;
+ 
+	    InputStream is = null;
 
-		InputStream is = null;
 		try {
+			url = new URL("http://39.152.129.170:10085/2/"+name+".m3u8");
+			is = url.openStream();
+			 
 
-			is = minioClient.getObject("movie", "1.mp4");
-
-			response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-			response.setHeader("Content-Disposition", "attachment; filename=" + name);
+			response.setContentType("application/vnd.apple.mpegurl");
+			response.setHeader("Content-Disposition", "attachment; filename=" + name+".m3u8");
 
 			IOUtils.copy(is, response.getOutputStream());
 			response.flushBuffer();
@@ -69,5 +56,30 @@ public class ContentController {
 		}
 
 	}
-	
+	 
+		@RequestMapping("/movie/{name}.ts")
+		public void  m(@PathVariable("name") String name,  HttpServletRequest request,
+				HttpServletResponse response) throws Exception {
+
+		    URL url = null;
+	 
+		    InputStream is = null;
+
+			try {
+				url = new URL("http://39.152.129.170:10085/2/"+name+".ts");
+				is = url.openStream();
+				 
+
+				response.setContentType("video/mp2t");
+				response.setHeader("Content-Disposition", "attachment; filename=" + name+".ts");
+
+				IOUtils.copy(is, response.getOutputStream());
+				response.flushBuffer();
+			} catch (java.nio.file.NoSuchFileException e) {
+				response.setStatus(HttpStatus.NOT_FOUND.value());
+			} catch (Exception e) {
+				response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			}
+
+		}
 }
