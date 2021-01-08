2445e739f1d415801e314abedc8ff943db2e9bef
1261954 <hengxie.che@oborcloud.com>
2021-01-08 10:51:51 +0800

    1

--- src/main/java/com/chea/cinema/controller/StreamController.java
+++ src/main/java/com/chea/cinema/controller/StreamController.java
@@ -8,18 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.chea.cinema.entity.FileType;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 public class StreamController {
 
 	private FileType ts = new FileType(".ts", "video/mp2t");
@@ -44,29 +43,29 @@ public class StreamController {
 		return version;
 	}
 
-	@RequestMapping("/movie/{name}.m3u8")
-	public void m3u8(@PathVariable("name") String name, @RequestParam String sign, HttpServletRequest request,
+	@RequestMapping("/movie/{name}/{index}.m3u8")
+	public void m3u8(@PathVariable("name") String name,@PathVariable("index") String index, @RequestParam String sign, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 
-		proxy(name, m3u8, response);
+		proxy(name,index ,m3u8, response);
 
 	}
 
-	@RequestMapping("/movie/{name}.ts")
-	public void ts(@PathVariable("name") String name, HttpServletRequest request, HttpServletResponse response)
+	@RequestMapping("/movie/{name}/{fragment}.ts")
+	public void ts(@PathVariable("name") String name,@PathVariable("fragment") String fragment,  HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 
-		proxy(name, ts, response);
-
+		proxy(name,fragment, ts, response);
+ 
 	}
 
-	public void proxy(String name, FileType fileType, HttpServletResponse response) throws Exception {
+	public void 	proxy(String name,String index,FileType fileType, HttpServletResponse response) throws Exception {
 
 		URL url = null;
 		InputStream is = null;
 
 		try {
-			url = new URL(targetUrl + name + "/1" + fileType.getSuffix());
+			url = new URL(targetUrl +  name + "/" +index+ fileType.getSuffix());
 			is = url.openStream();
 
 			response.setContentType(fileType.getContentType());
